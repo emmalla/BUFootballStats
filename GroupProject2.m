5f25724d934008b7a17bd9eab3e0c12367a9b768
Emily Mallaber <37315680+emmalla@users.noreply.github.com>
2019-11-07 16:02:05 -0500

    Update GroupProject2.m

--- GroupProject2.m
+++ GroupProject2.m
@@ -4,9 +4,24 @@
 rand_data = rand(1,50);
 randn_data = randn(1,50);
 bu_football_data = []; 
+bu_football_years = [];
 
 figure
-plot(rand_data);
+hist(rand_data);
 
 figure
-plot(randn_data);
+hist(randn_data);
+
+%% statistics
+
+rand_data_mean = mean(rand_data);
+randn_data_mean = mean(randn_data);
+bu_football_data = mean(bu_football_data);
+
+rand_data_median = median(rand_data);
+randn_data_median = median(randn_data);
+bu_football_data_median = median(bu_football_data);
+
+rand_data_std = std(rand_data);
+randn_data_std = std(randn_data);
+bu_football_data_std = std(bu_football_data);
